d50e9fb7d08d9634392fc402da85883fae79aac8
Nils Borgböhmer <nils@strandrover.com>
2019-01-15 13:01:59 +0100

    fixing random bugs (hahaha)

--- src/components/header.js
+++ src/components/header.js
@@ -10,28 +10,38 @@ const Image = ({ className }) => (
     query={graphql`
       fragment imageShit on File {
         childImageSharp {
-            fixed(height: 150) {
-              ...GatsbyImageSharpFixed
-            }
+          fixed(height: 150) {
+            ...GatsbyImageSharpFixed
           }
+        }
       }
       query {
-        image0: file(relativePath: { eq: "wallpapers/DSC07590.jpg" }) { ...imageShit }
-        image1: file(relativePath: { eq: "wallpapers/DSC08409.jpg" }) { ...imageShit }
-        image2: file(relativePath: { eq: "wallpapers/IMG_4067.jpg" }) { ...imageShit }
-        image3: file(relativePath: { eq: "wallpapers/IMG_4602.jpg" }) { ...imageShit }
-        image4: file(relativePath: { eq: "wallpapers/IMG_4637.jpg" }) { ...imageShit }
-        image5: file(relativePath: { eq: "wallpapers/IMG_5941.jpg" }) { ...imageShit }
+        image0: file(relativePath: { eq: "wallpapers/DSC07590.jpg" }) {
+          ...imageShit
+        }
+        image1: file(relativePath: { eq: "wallpapers/DSC08409.jpg" }) {
+          ...imageShit
+        }
+        image2: file(relativePath: { eq: "wallpapers/IMG_4067.jpg" }) {
+          ...imageShit
+        }
+        image3: file(relativePath: { eq: "wallpapers/IMG_4602.jpg" }) {
+          ...imageShit
+        }
+        image4: file(relativePath: { eq: "wallpapers/IMG_4637.jpg" }) {
+          ...imageShit
+        }
+        image5: file(relativePath: { eq: "wallpapers/IMG_5941.jpg" }) {
+          ...imageShit
+        }
       }
     `}
     render={data => {
-      const image = data[`image${Math.floor(Math.random() * 6)}`]
-      return (
-        <Img
-          className={className}
-          fixed={image.childImageSharp.fixed}
-        />
-      )
+      const images = Object.values(data).map(image => (
+        <Img className={className} fixed={image.childImageSharp.fixed} />
+      ))
+
+      return images[Math.floor(Math.random() * 6)]
     }}
   />
 )
